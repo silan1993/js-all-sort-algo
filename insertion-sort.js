fc6f44b0ada6b5f46490fcf9195d22c89e7b6afa
silansubhra <subhrajyoti1903@gmail.com>
2021-07-20 22:56:08 +0530

    added insertiomn issue fix

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,7 +1,7 @@
 let insertionsort = arr => {
     for (let i = 1; i < arr.length; i++) {
         const element = arr[i];
-        for (let j = i - 1; j >= 0 && arr[j] > element; j--) {
+        for (var j = i - 1; j >= 0 && arr[j] > element; j--) {
             arr[j+1] = arr[j]
             
         }
@@ -12,3 +12,5 @@ let insertionsort = arr => {
 
 
 // here time complexcity is O(N) in worst case it will be O(N^2) if data is quadratic
+
+insertionsort([2,3,56,7,9,6,5])
